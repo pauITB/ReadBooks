5c25aa48edd948461abb7660fdf8cc62c41fd002
pauITB <73183443+pauITB@users.noreply.github.com>
2021-04-27 15:02:25 +0200

    Edit_chack

--- app/src/androidTest/java/cat/itb/readbooks/Activities/NavigationTest.java
+++ app/src/androidTest/java/cat/itb/readbooks/Activities/NavigationTest.java
@@ -2,11 +2,19 @@ package cat.itb.readbooks.Activities;
 
 
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
 import androidx.test.runner.AndroidJUnit4;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,6 +32,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -36,7 +45,12 @@ public class NavigationTest {
     public void login_go_GridFragment_go_EditFragment(){
         loginFragment_to_GridFragment();
         onView(withId(R.id.floatingActionButton)).perform(click());
-        onView(withId(R.id.rvBooks)).perform(actionOnItemAtPosition(1,click()));
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.rvBooks),
+                        childAtPosition(
+                                withId(R.id.booksGridFragment),
+                                0)));
+        recyclerView.perform(actionOnItemAtPosition(1, click()));
         onView(withId(R.id.edit_book_fragment)).check(matches(isDisplayed()));
     }
 
@@ -67,6 +81,25 @@ public class NavigationTest {
         onView(withId(R.id.password_loginET)).perform(typeText("Passwordos")).perform(closeSoftKeyboard());
         onView(withId(R.id.login_button_login)).perform(click());
         onView(withId(R.id.booksGridFragment)).check(matches(isCompletelyDisplayed()));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 
 }
--- app/src/androidTest/java/cat/itb/readbooks/UseCaseTest.java
+++ app/src/androidTest/java/cat/itb/readbooks/UseCaseTest.java
@@ -1,16 +1,25 @@
 package cat.itb.readbooks;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import cat.itb.readbooks.Activities.MainActivity;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -21,6 +30,11 @@ import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(AndroidJUnit4.class)
 public class UseCaseTest {
@@ -60,7 +74,53 @@ public class UseCaseTest {
         onView(withId(R.id.autor_edit_text)).perform(typeText("Gepeto"));
         onView(withId(R.id.add_button)).perform(click());
         onView(withId(R.id.booksGridFragment)).check(matches(isCompletelyDisplayed()));
-        onView(withId(R.id.rvBooks)).perform(RecyclerViewActions.actionOnItemAtPosition(2,click()));
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.rvBooks),
+                        childAtPosition(
+                                withId(R.id.booksGridFragment),
+                                0)));
+        recyclerView.perform(actionOnItemAtPosition(2, click()));
+    }
+
+    @Test
+    public void  editBook_check(){
+        login_Check();
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.rvBooks),
+                        childAtPosition(
+                                withId(R.id.booksGridFragment),
+                                0)));
+        recyclerView.perform(actionOnItemAtPosition(1, click()));
+        onView(withId(R.id.edit_book_fragment)).check(matches(isDisplayed()));
+        onView(withId(R.id.status_spinner)).perform();
+        onView(withId(R.id.status_spinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Read"))).perform(click());
+        onView(withId(R.id.status_spinner)).check(matches(withSpinnerText(containsString("Read"))));
+        onView(withId(R.id.edit_button)).perform(click());
+    }
+
+
+
+
+
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 
 }
